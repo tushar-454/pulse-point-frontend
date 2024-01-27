a415e0fca30fe22d68492f4da76538c039f82f3f
tushar-454 <imtushar454@gmail.com>
2024-01-27 19:36:53 +0600

    information cards future management increase using loop to show all informtaion from array

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,5 +1,30 @@
+'use client';
+import { FaRegClock } from 'react-icons/fa';
+import { HiOutlineLocationMarker } from 'react-icons/hi';
+import { VscCallOutgoing } from 'react-icons/vsc';
 import InformationCard from './InformationCard';
 import Container from './shared/Container';
+
+const informations = [
+  {
+    id: 0,
+    icon: <FaRegClock className='relative top-1 text-5xl' />,
+    infoTitle: 'Opening Hours',
+    infoDesc: 'Open 9.00 am to 5.00pm Everyday',
+  },
+  {
+    id: 1,
+    icon: <HiOutlineLocationMarker className='relative top-1 text-5xl' />,
+    infoTitle: 'Our Locations',
+    infoDesc: 'Dhanmondi 17, Dhaka -1200, Bangladesh',
+  },
+  {
+    id: 2,
+    icon: <VscCallOutgoing className='relative top-1 text-5xl' />,
+    infoTitle: 'Contact Us',
+    infoDesc: '+8801750000000 +8801750000000',
+  },
+];
 
 const Information = () => {
   return (
@@ -7,9 +32,9 @@ const Information = () => {
       <Container>
         {/* information cards wrapper  */}
         <div className='mb-20 flex flex-col justify-between gap-10 lg:flex-row'>
-          <InformationCard />
-          <InformationCard />
-          <InformationCard />
+          {informations.map((information) => (
+            <InformationCard key={information.id} information={information} />
+          ))}
         </div>
       </Container>
     </section>
--- src/components/InformationCard.jsx
+++ src/components/InformationCard.jsx
@@ -1,16 +1,20 @@
-import { FaRegClock } from 'react-icons/fa';
-
-const InformationCard = () => {
+const InformationCard = ({ information }) => {
+  const { id, icon, infoTitle, infoDesc } = information;
   return (
-    <div className='bg-hit-pink-300 flex gap-3 rounded-lg p-10 text-white'>
+    <div
+      className={`bg-hit-pink-300 flex gap-5 rounded-lg p-10 text-white ${
+        id % 2 === 0 ? 'bg-deep-teal-800' : 'bg-hit-pink-400'
+      }`}
+    >
       {/* icon  */}
       <div>
-        <FaRegClock className='relative top-1 text-5xl' />
+        {/* <FaRegClock className='relative top-1 text-5xl' /> */}
+        {icon}
       </div>
       {/* information text content  */}
       <div className='space-y-2'>
-        <h1 className='text-2xl font-bold'>Opening Hour</h1>
-        <p className='text-xl font-normal'>Open 9.00 am to 5.00pm Everyday</p>
+        <h1 className='text-2xl font-bold'>{infoTitle}</h1>
+        <p className='text-xl font-normal'>{infoDesc}</p>
       </div>
     </div>
   );
