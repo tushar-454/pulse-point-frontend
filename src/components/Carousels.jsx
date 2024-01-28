c1127860d952a733c0a4ae790538db74cf39b738
tushar-454 <imtushar454@gmail.com>
2024-01-28 22:33:07 +0600

    Add testimonials to Carousels component and update TestimonialCard component

--- src/components/Carousels.jsx
+++ src/components/Carousels.jsx
@@ -1,8 +1,64 @@
 'use client';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa6';
 import TestimonialCard from './TestimonialCard';
-const testimonials = [{}, {}, {}, {}, {}, {}, {}, {}];
+const testimonials = [
+  {
+    id: 1,
+    patientImg:
+      'https://www.mountaingoatsoftware.com/uploads/training/brian-bg.jpg',
+    patientName: 'John Doe',
+    date: '2024-01-28',
+    comment:
+      "Patient John Doe has been diligently following the prescribed medication regimen post-surgery. He reports minimal discomfort and credits the medications for managing pain effectively. John expresses gratitude for the medical team's guidance and is pleased with the positive impact on his recovery. The prescribed medications have played a crucial role in alleviating post-operative symptoms, allowing John to focus on rehabilitation and regain strength. Regular check-ins with the healthcare provider ensure that any adjustments to the medication plan are promptly addressed. Overall, John is optimistic about the continued positive influence of the prescribed medications on his health.",
+  },
+  {
+    id: 2,
+    patientImg:
+      'https://images.ctfassets.net/lh3zuq09vnm2/yBDals8aU8RWtb0xLnPkI/19b391bda8f43e16e64d40b55561e5cd/How_tracking_user_behavior_on_your_website_can_improve_customer_experience.png',
+    patientName: 'Jane Smith',
+    date: '2024-01-29',
+    comment:
+      "Jane Smith shares her experience with prescribed medications following recent treatment. She reports noticeable improvements in symptoms, attributing the positive changes to the prescribed medications. Jane appreciates the personalized approach to her care and the effectiveness of the prescribed regimen. The medical team's thorough explanation of each medication has empowered Jane to manage her health confidently. Scheduled medication reviews and follow-up appointments contribute to Jane's ongoing well-being. She acknowledges the importance of medication adherence and values the collaborative effort with her healthcare provider. Overall, Jane's health journey is positively influenced by the prescribed medications and ongoing medical support.",
+  },
+  {
+    id: 3,
+    patientImg:
+      'https://www.jsums.edu/wbc/files/2021/09/businesswoman-scaled.jpg',
+    patientName: 'Bob Johnson',
+    date: '2024-01-30',
+    comment:
+      "Bob Johnson provides insight into his experience with prescribed medications. Since starting the prescribed regimen, Bob has noticed a significant reduction in symptoms and improved overall well-being. He commends the healthcare team for their expertise in tailoring the medication plan to address his specific health needs. Bob emphasizes the importance of open communication with his healthcare provider, allowing for timely adjustments to the prescribed medications. Regular follow-up appointments ensure that Bob's health is continually monitored, providing reassurance and confidence in his treatment plan. Overall, Bob credits the prescribed medications as a key factor in his positive health trajectory and ongoing recovery.",
+  },
+  {
+    id: 4,
+    patientImg:
+      'https://images.unsplash.com/photo-1566753323558-f4e0952af115?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWFsZXxlbnwwfHwwfHx8MA%3D%3D',
+    patientName: 'Alice Williams',
+    date: '2024-02-01',
+    comment:
+      "Alice Williams shares her positive experience with prescribed medications. Since starting the medication plan, Alice has noticed a significant improvement in her symptoms. The carefully selected medications have effectively managed her condition, allowing her to resume daily activities with increased comfort. Alice appreciates the personalized approach taken by her healthcare provider, ensuring the prescribed medications align with her health goals. Ongoing communication and regular check-ins contribute to Alice's confidence in her treatment plan. She expresses gratitude for the positive impact of the prescribed medications on her overall well-being and quality of life.",
+  },
+  {
+    id: 5,
+    patientImg:
+      'https://img.buzzfeed.com/buzzfeed-static/complex/images/jyu7r1ssdhxtwcxikucy/method-man.jpg?output-format=jpg&output-quality=auto',
+    patientName: 'Ethan Martinez',
+    date: '2024-02-02',
+    comment:
+      'Ethan Martinez provides an update on his health journey with prescribed medications. Following the advice of his healthcare provider, Ethan has diligently taken the prescribed medications as part of his treatment plan. The positive effects are evident, with a reduction in symptoms and improved energy levels. Ethan acknowledges the importance of medication adherence and values the collaborative approach to his care. Regular follow-up appointments and adjustments to the medication plan based on his progress further emphasize the personalized nature of his treatment. Ethan remains optimistic about the continued positive impact of the prescribed medications on his health and overall recovery.',
+  },
+  {
+    id: 6,
+    patientImg:
+      'https://img.freepik.com/free-photo/freedom-concept-with-hiker-mountain_23-2148107064.jpg',
+    patientName: 'Olivia Brown',
+    date: '2024-02-03',
+    comment:
+      "Olivia Brown shares her experience with prescribed medications and their role in managing her health condition. Since incorporating the prescribed medications into her daily routine, Olivia has experienced a notable improvement in symptom control. The medications have provided relief, allowing Olivia to engage in daily activities more comfortably. She emphasizes the importance of the healthcare provider's guidance in selecting medications tailored to her specific needs. Olivia values the ongoing support and regular check-ins, ensuring that her treatment plan aligns with her evolving health status. Overall, Olivia expresses gratitude for the positive impact of prescribed medications on her health and well-being.",
+  },
+];
+
 const Carousels = () => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [percentage, setPercentage] = useState(50);
@@ -48,7 +104,7 @@ const Carousels = () => {
   return (
     <div className='relative overflow-hidden py-10'>
       {/* controller  */}
-      <div className='sliderContainer absolute top-48 z-50 flex w-full items-center justify-between'>
+      <div className='sliderContainer absolute left-[50%] top-[50%] z-50 flex w-full translate-x-[-50%] translate-y-[-50%] items-center justify-between'>
         <button onClick={prevSlide}>
           <FaChevronLeft className='h-14 w-14 rounded-full border-2 border-hit-pink-300 bg-white p-3 text-3xl text-hit-pink-400 transition hover:bg-hit-pink-300 hover:text-white active:bg-hit-pink-400 active:text-white' />
         </button>
--- src/components/TestimonialCard.jsx
+++ src/components/TestimonialCard.jsx
@@ -1,8 +1,8 @@
 import qouta from '@/assets/images/qouta.svg';
-import user from '@/assets/images/user.png';
 import Image from 'next/image';
 
 const TestimonialCard = ({ testimonial }) => {
+  const { patientImg, patientName, date, comment } = testimonial;
   return (
     <div
       className={`min-w-full space-y-4 rounded-lg border p-8 lg:min-w-[50%]`}
@@ -10,25 +10,23 @@ const TestimonialCard = ({ testimonial }) => {
       {/* Testimonial giver info  */}
       <div className='flex justify-between'>
         <div className='flex items-center gap-4'>
-          <Image src={user} alt='user' />
+          <Image
+            src={patientImg}
+            width={500}
+            height={500}
+            alt='user'
+            className='h-16 w-16 rounded-full object-cover'
+          />
           <div>
-            <h1 className='text-xl font-bold'>Awlad Hossain</h1>
-            <p className='text-base font-normal'>Product Designer</p>
+            <h1 className='text-xl font-bold'>{patientName}</h1>
+            <p className='text-base font-normal'>{date}</p>
           </div>
         </div>
         <div>
           <Image src={qouta} alt='quota icon' />
         </div>
       </div>
-      <p className='text-lg font-normal'>
-        Explore firsthand accounts of transformative experiences from our valued
-        patients. From regained confidence to renewed well-being, our
-        testimonials capture the impact of our dedicated care and personalized
-        approach. Join the voices of satisfaction and let their stories inspire
-        your journey toward health and happiness. At [Your Medical Help Center],
-        we take pride in the stories that reflect our commitment to excellence
-        and the positive impact we make in the lives of those we serve.
-      </p>
+      <p className='text-lg font-normal'>{comment}</p>
     </div>
   );
 };
