9926eb7dcde9b3896c10cee5e7708b602bde98b2
tushar-454 <imtushar454@gmail.com>
2024-01-30 23:47:50 +0600

    Fix login page and create UI signup page

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -4,21 +4,28 @@ import Button from '@/components/ui/Button';
 import Input from '@/components/ui/Input';
 import Link from 'next/link';
 
-const Login = () => {
+const Signup = () => {
   return (
     <main className='bg-slate-50'>
       <Container>
-        {/* login wrapper  */}
+        {/* signup wrapper  */}
         <div className='flex min-h-screen justify-center py-20'>
           <div className='h-fit w-full rounded-xl border bg-white p-8 sm:w-[640px]'>
             <h1 className='my-10 text-center text-3xl font-bold'>
-              Login to Pulse Point
+              Signup to Pulse Point
             </h1>
             {/* form start  */}
             <form className='space-y-5'>
               <Input
-                label={'Email'}
+                label={'Full Name'}
                 type={'text'}
+                id={'name'}
+                name={'name'}
+                placeholder={'Dr. John Doe'}
+              />
+              <Input
+                label={'Email'}
+                type={'enail'}
                 id={'email'}
                 name={'email'}
                 placeholder={'example@text.com'}
@@ -31,16 +38,24 @@ const Login = () => {
                 isPassword={true}
                 placeholder={'H*&%FIUHsdfh'}
               />
-              <Button style={{ width: '100%' }}>Login account</Button>
+              <Input
+                label={'Confirm Password'}
+                type={'password'}
+                id={'confirmPassword'}
+                name={'confirmPassword'}
+                isPassword={true}
+                placeholder={'H*&%FIUHsdfh'}
+              />
+              <Button style={{ width: '100%' }}>Create an account</Button>
             </form>
             <LoginwithGoogle />
             <p className='my-8 text-center text-lg font-bold'>
-              Please register at first. Go to{' '}
+              Already have an account? Go to{' '}
               <Link
-                href={'/signup'}
+                href={'/login'}
                 className='text-hit-pink-300 hover:underline'
               >
-                SIGN UP
+                LOG IN
               </Link>
             </p>
           </div>
@@ -50,4 +65,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Signup;
