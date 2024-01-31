409de42f422ca1597c1a2a7987d89068e8623163
tushar-454 <imtushar454@gmail.com>
2024-01-31 07:40:21 +0600

    Update header navigation icons and links

--- src/components/HeaderNavItems.jsx
+++ src/components/HeaderNavItems.jsx
@@ -1,11 +1,12 @@
 'use client';
 import Link from 'next/link';
 import { useState } from 'react';
+import { BiSolidContact } from 'react-icons/bi';
 import { BsFillMenuButtonWideFill } from 'react-icons/bs';
+import { FaUserDoctor } from 'react-icons/fa6';
 import { GiTireIronCross } from 'react-icons/gi';
 import { IoHomeSharp } from 'react-icons/io5';
 import { MdRoundaboutLeft } from 'react-icons/md';
-import { SiGooglemeet } from 'react-icons/si';
 import { TbLogin2 } from 'react-icons/tb';
 
 const navItems = [
@@ -23,15 +24,15 @@ const navItems = [
   },
   {
     id: 2,
-    name: 'Appointment',
-    link: '/appointment',
-    icon: <SiGooglemeet />,
+    name: 'Doctors',
+    link: '/doctors',
+    icon: <FaUserDoctor />,
   },
   {
     id: 3,
-    name: 'Login',
-    link: '/login',
-    icon: <TbLogin2 />,
+    name: 'Contact Us',
+    link: '/contact',
+    icon: <BiSolidContact />,
   },
 ];
 
@@ -54,18 +55,24 @@ const HeaderNavItems = () => {
       <nav
         className={`${
           showNav ? 'undo-transform-custom' : 'transform-custom'
-        } absolute left-0 top-24 flex w-full flex-col justify-center gap-5 rounded-lg bg-deep-teal-950/50 p-4 backdrop-blur-lg transition duration-300 md:static md:left-0 md:top-0 md:w-fit md:flex-row md:rounded-none md:bg-slate-100/0 md:p-0`}
+        } absolute left-0 top-24 flex w-full flex-col justify-center gap-3 rounded-lg bg-deep-teal-950/50 p-4 backdrop-blur-lg transition duration-300 md:static md:left-0 md:top-0 md:w-fit md:flex-row md:rounded-none md:bg-slate-100/0 md:p-0`}
       >
         {navItems.map((item, index) => (
           <Link
             key={index}
             href={item.link}
-            className='flex w-fit items-center gap-2 rounded-full bg-gradient-to-l from-hit-pink-300 to-deep-teal-400 px-6 py-1 font-semibold text-white transition duration-100 ease-in-out hover:scale-105 hover:bg-gradient-to-r'
+            className='flex w-fit items-center gap-2 rounded-full bg-gradient-to-l from-hit-pink-300 to-deep-teal-400 px-5 py-1 font-semibold text-white transition duration-100 ease-in-out hover:scale-105 hover:bg-gradient-to-r'
           >
             {item.icon}
             {item.name}
           </Link>
         ))}
+        <Link
+          href={'/login'}
+          className='flex w-fit items-center gap-2 rounded-full bg-gradient-to-l from-hit-pink-300 to-deep-teal-400 px-5 py-1 font-semibold text-white transition duration-100 ease-in-out hover:scale-105 hover:bg-gradient-to-r'
+        >
+          <TbLogin2 /> Login
+        </Link>
       </nav>
     </>
   );
